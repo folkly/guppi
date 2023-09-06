2882404eb4c474865e9de82a68daa4b5452319c4
folkly-dev <folkly.dev@gmail.com>
2023-09-06 00:20:54 -0400

    feat: add icons

---
+++ src/main/java/io/github/folkly/guppi/MyIcons.java
@@ -0,0 +1,12 @@
+package io.github.folkly.guppi;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public class MyIcons {
+    Icon guppiIcon = IconLoader.getIcon("pluginIcon.svg", MyIcons.class);
+    Icon guppiIconOrange = IconLoader.getIcon("pluginIcon.svg", MyIcons.class);
+    Icon guppiIconLightGrey = IconLoader.getIcon("pluginIcon_lightGrey.svg", MyIcons.class);
+    Icon guppiIconDarkGrey = IconLoader.getIcon("pluginIcon_darkGrey.svg", MyIcons.class);
+}
